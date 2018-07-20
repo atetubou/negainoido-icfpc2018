4740fea6424e9c50950fbdc16993f6b682a78aa3
Shuichi <tailed2341@gmail.com>
2018-07-21 08:57:57 +0900

    totyu

---
+++ src/skeleton.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "gflags/gflags.h"
+#include "glog/logging.h"
+
+#include "src/base/base.h"
+
+DEFINE_string(mdl_filename, "", "filepath of mdl");
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (FLAGS_mdl_filename.empty()) {
+    std::cout << "need to pass --mdl_filename=/path/to/mdl";
+    exit(1);
+  }
+
+  vvv M = ReadMDL(FLAGS_mdl_filename);
+  int R = M.size();
+  std::cout << R << std::endl;
+
+  for (int x=0;x<R;++x) {
+    for (int y=0; y<R; ++y) {
+      for (int z=0;z<R;++z) {
+        std::cout << M[x][y][z];
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+}
