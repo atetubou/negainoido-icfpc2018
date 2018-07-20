727479fc1e0a08e6bc4b254749b2604949ec5306
garasubo <garasubo@gmail.com>
2018-07-21 00:06:14 +0900

    add css setting

--- sanaki/src/index.js
+++ sanaki/src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
+import '../styles/main.scss';
+import Icon from '../assets/garasubo.jpg';
 
-function component() {
-  var element = document.createElement('div');
+const component = () => {
+  let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  let img = new Image();
+  img.src = Icon;
+  img.className = 'garasubo';
+
+  element.appendChild(img);
+
   return element;
-}
+};
 
 console.log('java');
 
--- sanaki/webpack.config.js
+++ sanaki/webpack.config.js
@@ -6,5 +6,36 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /styles\/.+\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+              minimize: true,
+              importLoaders: 2
+            },
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /assets\/.+\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    publicPath: '/'
   }
 };
