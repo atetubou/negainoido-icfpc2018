f12b4ce253094ec5b1537b6abb6d0a80c16e58a4
Keiichi Watanabe <udon.watanabe@gmail.com>
2018-07-21 05:00:29 +0900

    command

--- src/command_executer.cc
+++ src/command_executer.cc
@@ -1,5 +1,30 @@
 #include "command_executer.h"
 
+#define UNREACHABLE() CHECK(false)
+
+CommandExecuter::CommandExecuter(int R) {}
+
+Point Point::operator+ (const Point& p) {
+  return Point(x + p.x, y + p.y, z + p.z);
+}
+
+Point& Point::operator+= (const Point& p) {
+  this->x += p.x;
+  this->y += p.y;
+  this->z += p.z;
+  return *this;
+}
+
+bool operator==(const Point& p1, const Point& p2) {
+    return
+      p1.x == p2.x &&
+      p1.y == p2.y &&
+      p1.z == p2.z;
+}
+bool operator!=(const Point& p1, const Point& p2) {
+    return !(p1 == p2);
+}
+
 bool IsLCD(const Point& p) {
   if ((p.x == 0 && p.y != 0 && p.z != 0) ||
       (p.x != 0 && p.y == 0 && p.z != 0) ||
@@ -11,14 +36,6 @@ bool IsLCD(const Point& p) {
 
 uint32_t MLen(const Point& p) {
   return abs(p.x) + abs(p.y) + abs(p.z);
-}
-
-uint32_t GetBotsNum() {
-  return system_status.bot_status.size();
-}
-
-bool IsValidBotId(const uint32_t id) {
-  return 1 <= id && id <= GetBotsNum();
 }
 
 bool IsSLD(const Point& p) {
@@ -37,14 +54,22 @@ bool IsPath(const Point& p1, const Point& p2) {
 }
 
 
+uint32_t CommandExecuter::GetBotsNum() {
+  return system_status.active_bots.size();
+}
+
+bool CommandExecuter::IsValidBotId(const uint32_t id) {
+  return 1 <= id && id <= GetBotsNum();
+}
+
 // Check if
 //  * [p1, p2] is a path and
 //  * every coordinate in the region [p1,p2] is Void
-bool IsVoidPath(const Point& p1, const Point& p2) {
+bool CommandExecuter::IsVoidPath(const Point& p1, const Point& p2) {
   if (!IsPath(p1, p2))
     return false;
 
-  Point delta;
+  Point delta(0,0,0);
 
   if (p1.x != p2.x) {
     int dx = (p1.x < p2.x) ? 1 : -1;
@@ -53,7 +78,7 @@ bool IsVoidPath(const Point& p1, const Point& p2) {
     int dy = (p1.y < p2.y) ? 1 : -1;
     delta = Point(0, dy, 0);
   } else if (p1.z != p2.z) {
-    int dz = (p1.y < p2.y) ? 1 : -1;
+    int dz = (p1.z < p2.z) ? 1 : -1;
     delta = Point(0, 0, dz);
   } else {
     UNREACHABLE();
@@ -70,27 +95,38 @@ bool IsVoidPath(const Point& p1, const Point& p2) {
   return true;
 }
 
-void Wait(const uint32_t bot_id) {
+void CommandExecuter::Halt(const uint32_t bot_id) {
   CHECK(IsValidBotId(bot_id));
+  Point& p = bot_status[bot_id].pos;
+  CHECK(p.x == 0 && p.y == 0 && p.z == 0);
+  CHECK(GetBotsNum() == 1);
+  CHECK(system_status.active_bots.find(bot_id) != system_status.active_bots.end());
+  CHECK(system_status.harmonics == LOW);
 
-  Point c = bot_status[bot_id].pos;
-  v_cords.push(make_pair(c, c));
+  system_status.active_bots.empty();
 }
 
-void Flip(const uint32_t bot_id) {
+void CommandExecuter::Wait(const uint32_t bot_id) {
   CHECK(IsValidBotId(bot_id));
 
   Point c = bot_status[bot_id].pos;
-  v_cords.push(make_pair(c, c));
+  v_cords.push_back(std::make_pair(c, c));
+}
 
-  if (system_status.harmonics == High) {
-    system_status.harmonics = Low;
+void CommandExecuter::Flip(const uint32_t bot_id) {
+  CHECK(IsValidBotId(bot_id));
+
+  Point c = bot_status[bot_id].pos;
+  v_cords.push_back(std::make_pair(c, c));
+
+  if (system_status.harmonics == HIGH) {
+    system_status.harmonics = LOW;
   } else {
-    system_status.harmonics = High;
+    system_status.harmonics = HIGH;
   }
 }
 
-void SMove(const uint32_t bot_id, const Point& lld) {
+void CommandExecuter::SMove(const uint32_t bot_id, const Point& lld) {
   CHECK(IsValidBotId(bot_id));
   CHECK(IsLLD(lld));
 
@@ -101,11 +137,11 @@ void SMove(const uint32_t bot_id, const Point& lld) {
 
   CHECK(IsVoidPath(c0, c1));
 
-  v_cords.push(make_pair(c0, c1));
+  v_cords.push_back(std::make_pair(c0, c1));
   system_status.energy += 2 * MLen(lld);
 }
 
-void LMove(const uint32_t bot_id, const Point& sld1, const Point& sld2) {
+void CommandExecuter::LMove(const uint32_t bot_id, const Point& sld1, const Point& sld2) {
   CHECK(IsValidBotId(bot_id));
   CHECK(IsSLD(sld1));
   CHECK(IsSLD(sld2));
@@ -118,7 +154,7 @@ void LMove(const uint32_t bot_id, const Point& sld1, const Point& sld2) {
   CHECK(IsVoidPath(c0, c1));
   CHECK(IsVoidPath(c1, c2));
 
-  v_cords.push(make_pair(c0, c1));
-  v_cords.push(make_pair(c1, c2));
+  v_cords.push_back(std::make_pair(c0, c1));
+  v_cords.push_back(std::make_pair(c1, c2));
   system_status.energy += 2 * (MLen(sld1) + 2 + MLen(sld2));
 }
--- src/command_executer.h
+++ src/command_executer.h
@@ -11,11 +11,15 @@ struct Point {
   int x;
   int y;
   int z;
+  Point() = default;
+  Point(int x, int y, int z) : x(x), y(y), z(z) {}
   Point operator+ (const Point& p);
   Point& operator+= (const Point& p);
 };
+bool operator==(const Point& p1, const Point& p2);
+bool operator!=(const Point& p1, const Point& p2);
 
-enum Harmonic {
+enum Harmonics {
   LOW = 0,
   HIGH = 1,
 };
@@ -32,30 +36,45 @@ class CommandExecuter {
     kMaxResolution = 250,
   };
   struct SystemStatus {
-    const int R;
+    const int R = 0;
     int energy;
-    Harmonic harmonic;
+    Harmonics harmonics;
     VoxelState matrix[kMaxResolution][kMaxResolution][kMaxResolution];
     // TODO(hiroh): represent active bot in more efficient way.
     // might be, bool active_bots[kMaxNumBots + 1];
     std::set<int> active_bots;
+    SystemStatus() = default;
   };
   struct BotStatus {
     int id; // 1-indexed!
     Point pos;
     std::pair<int,int> seeds; // [x, y)
+    BotStatus() = default;
   };
 
   CommandExecuter(int R);
   ~CommandExecuter() = default;
-  // Func()
-  // void LMove(id, sld1, sld2);
-  //
+
+  // Commands
+  // Singleton Commands
+  void Halt(const uint32_t bot_id);
+  void Wait(const uint32_t bot_id);
+  void Flip(const uint32_t bot_id);
+  void SMove(const uint32_t bot_id, const Point& lld);
+  void LMove(const uint32_t bot_id, const Point& sld1, const Point& sld2);
+  // TODO(udon)
 
  private:
   std::vector<std::pair<Point, Point>> v_cords;
   BotStatus bot_status[kMaxNumBots];
   SystemStatus system_status;
+
+  // utility
+  uint32_t GetBotsNum();
+  bool IsValidBotId(const uint32_t id);
+  bool IsVoidPath(const Point& p1, const Point& p2);
+
+
 };
 
 #endif
