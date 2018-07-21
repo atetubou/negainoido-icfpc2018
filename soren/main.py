4ef18c8660050df39380ca91940e6ff5a2f81125
autotaker <autotaker@gmail.com>
2018-07-22 07:36:23 +0900

    quit the driver

--- soren/main.py
+++ soren/main.py
@@ -37,47 +37,49 @@ options.add_argument('--headless')
 # ChromeのWebDriverオブジェクトを作成する。
 driver = Chrome(options=options)
 
-driver.get(url)
+try:
+    driver.get(url)
 
-assert 'ICFP' in driver.title
+    assert 'ICFP' in driver.title
 
 # find input model file
-if src != None:
-    src_input = driver.find_element_by_id('srcModelFileIn')
-    src_input.send_keys(src)
-else:
-    driver.find_element_by_id('srcModelEmpty').click()
+    if src != None:
+        src_input = driver.find_element_by_id('srcModelFileIn')
+        src_input.send_keys(src)
+    else:
+        driver.find_element_by_id('srcModelEmpty').click()
 
-if tgt != None:
-    model_input = driver.find_element_by_id('tgtModelFileIn')
-    model_input.send_keys(tgt)
-else:
-    driver.find_element_by_id('tgtModelEmpty').click()
+    if tgt != None:
+        model_input = driver.find_element_by_id('tgtModelFileIn')
+        model_input.send_keys(tgt)
+    else:
+        driver.find_element_by_id('tgtModelEmpty').click()
 
-tracer_input = driver.find_element_by_id('traceFileIn')
-tracer_input.send_keys(nbt)
-driver.find_element_by_id('execTrace').click()
+    tracer_input = driver.find_element_by_id('traceFileIn')
+    tracer_input.send_keys(nbt)
+    driver.find_element_by_id('execTrace').click()
 
-wait_limit = 30
-stdout_text = ''
-while wait_limit > 0:
-    stdout_text = driver.find_element_by_id('stdout').text
-    if stdout_text.find('Success') >= 0:
-        break
-    if stdout_text.find('Failure') >= 0:
-        print('Found failure: ' + stdout_text)
+    wait_limit = 30
+    stdout_text = ''
+    while wait_limit > 0:
+        stdout_text = driver.find_element_by_id('stdout').text
+        if stdout_text.find('Success') >= 0:
+            break
+        if stdout_text.find('Failure') >= 0:
+            print('Found failure: ' + stdout_text)
+            exit(1)
+        sleep(1)
+        wait_limit -= 1
+
+    energy = re.search('Energy:\s+([0-9]+)\n', stdout_text)
+    if energy == None:
+        print('timeout')
         exit(1)
-    sleep(1)
-    wait_limit -= 1
 
-energy = re.search('Energy:\s+([0-9]+)\n', stdout_text)
-if energy == None:
-    print('timeout')
-    exit(1)
-
-print(energy.group(1))
-
+    print(energy.group(1))
 # スクリーンショットを撮る。
 #driver.save_screenshot('tracer.png')
 
-driver.quit()  # ブラウザーを終了する
+finally:
+    driver.quit()  # ブラウザーを終了する
+
