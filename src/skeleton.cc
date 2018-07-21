d2efd106411dd94fdba79b9c85a3fb1ace4f522c
Shuichi <tailed2341@gmail.com>
2018-07-21 15:00:40 +0900

    completed

--- src/skeleton.cc
+++ src/skeleton.cc
@@ -5,6 +5,7 @@
 #include <queue>
 #include <map>
 #include <memory>
+#include <deque>
 using namespace std;
 
 #include "gflags/gflags.h"
@@ -46,6 +47,10 @@ vvv PointsToVVV(const Path &path, int R = -1) {
     M[v.x][v.y][v.z] = 1;
   }
   return M;
+}
+
+vvv InitVVV(int R) {
+  return vvv(R, vv(R, v(R, 0)));
 }
 
 class Skeleton {
@@ -135,13 +140,77 @@ public:
       }
     }
 
-    ce.SMove(1, Point(1, 0, 0));
+    FillAccordingToRank(rank, InitVVV(R), Point(0, 0, 0));
 
 
     OutputMDL(rank);
   }
 
-  void Enbody_sliced() {
+  void FillAccordingToRank(const vvv &rank, vvv wall, Point pos) {
+    deque<int> ranks;
+    for(int y=0; y<R; y++) {
+      for(int x=0; x<R; x++) {
+        for(int z=0; z<R; z++) {
+          if (rank[x][y][z])
+            ranks.push_back(rank[x][y][z]);
+        }
+      }
+    }
+    sort(ranks.begin(), ranks.end());
+
+    const Point fill_here(-1, -1, -1);
+    Path cmds;
+    while(!ranks.empty()) {
+      int target_rank = ranks.front();
+      ranks.pop_front();
+
+      map<Point, Point> prev;
+      queue<Point> q;
+      q.push(pos);
+      prev[pos] = pos;
+      while(!q.empty()) {
+        Point v = q.front();
+        q.pop();
+        if (rank[v.x][v.y][v.z] == target_rank) {
+          Path path;
+          Point w = v;
+          while(w != pos) {
+            path.push_back(w);
+            w = prev[w];
+          }
+          path.push_back(pos);
+          reverse(path.begin(), path.end());
+          cmds.insert(cmds.end(), path.begin(), path.end()); // append path
+          cmds.push_back(fill_here);
+
+          wall[v.x][v.y][v.z] = 1; // fill
+          pos = v;
+
+          break;
+        }
+        for(int dx=-1; dx<=1; dx++) {
+          for(int dy=-1; dy<=1; dy++) {
+            for(int dz=-1; dz<=1; dz++) {
+              if (abs(dx) + abs(dy) + abs(dz) != 1) continue;
+              Point w = v + Point(dx, dy, dz);
+              if (w.x < 0 || R <= w.x || w.y < 0 || R <= w.y || w.z < 0 || R <= w.z) continue;
+              if (wall[w.x][w.y][w.z]) continue;
+              if (prev.count(w)) continue;
+              prev[w] = v;
+              q.push(w);
+            }
+          }
+        }
+      }
+    }
+
+    for(int i=0; i<(int)cmds.size(); i++) {
+      cout << cmds[i] << endl;
+    }
+
+
+//    ce.SMove(1, Point(1, 0, 0));
+
   }
 
 };
