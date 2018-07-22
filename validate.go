11e64950a8e974d0bc50b82be8df50f6db52043b
tikuta <tikuta@tikuta-Lenovo-ideapad-710S-Plus-13IKB>
2018-07-22 16:48:33 +0900

    wip

--- validate.go
+++ validate.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 var (
-	problem = flag.String("problem", "LA001", `problem name e.g. "LA001"`)
+	problem = flag.String("problem", "FA001", `problem name e.g. "FA001"`)
 	solver  = flag.String("solver", "//solver:simple_solve", "ai target")
 )
 
@@ -27,23 +28,27 @@ func runCommandsOutput(command string) string {
 	return string(output)
 }
 
-func setupFiles() {
-	log.Print("Setup files")
-	runCommands("gsutil -m rsync -r gs://negainoido-icfpc2018-shared-bucket/problemsL shared")
-	runCommands("gsutil -m rsync -r gs://negainoido-icfpc2018-shared-bucket/dfltTracesL shared")
-}
-
 func main() {
 	flag.Parse()
 
-	setupFiles()
+	srcMDL := fmt.Sprintf("$(pwd)/shared/%s_src.mdl", *problem)
+	tgtMDL := fmt.Sprintf("$(pwd)/shared/%s_tgt.mdl", *problem)
 
-	runCommands(fmt.Sprintf("bazel run %s -- --mdl_filename=$(pwd)/shared/%s_tgt.mdl > /tmp/nbt.nbt", *solver, *problem))
-	simulatorResult := runCommandsOutput(fmt.Sprintf("bazel run //src:simulator -- --mdl_filename=$(pwd)/shared/%s_tgt.mdl --nbt_filename=/tmp/nbt.nbt", *problem))
+	if strings.HasPrefix("LA", *problem) || strings.HasPrefix("FA", *problem) {
+		srcMDL = "-"
+	}
+
+	if strings.HasPrefix("FD", *problem) {
+		tgtMDL = "-"
+	}
+
+	solveCommand := fmt.Sprintf("bazel run %s -- --mdl_filename=%s > /tmp/nbt.nbt", *solver, tgtMDL)
+	runCommands()
+	simulatorResult := runCommandsOutput(fmt.Sprintf("bazel run //src:simulator -- --mdl_filename=%s --nbt_filename=/tmp/nbt.nbt", tgtMDL))
 
 	log.Printf("Simulator Result %s", simulatorResult)
 
-	officialResult := runCommandsOutput(fmt.Sprintf("python3 soren/main.py - $(pwd)/shared/%s_tgt.mdl /tmp/nbt.nbt", *problem))
+	officialResult := runCommandsOutput(fmt.Sprintf("python3 soren/main.py %s %s /tmp/nbt.nbt", srcMDL, tgtMDL))
 
 	log.Printf("Official Result %s", officialResult)
 
