7d30555fd84898e9fb6b67f011d52887a0e649f5
Takanori Hayashi <flowlight0@gmail.com>
2018-07-22 04:38:11 +0000

    Parallelize run_solver scripts

--- run_solver_fa.sh
+++ run_solver_fa.sh
@@ -7,11 +7,10 @@ echo $MDL_PATH $OUT_DIR $SOLVER
 
 mkdir -p $OUT_DIR
 
+P=${P:-1}
 for mdl in $MDL_PATH/FA*.mdl; do
     if [ -f $mdl ]; then
         output_file=`basename $mdl | sed -e 's/_tgt\.mdl/\.nbt/'`
-        echo "running for ${mdl}"
-        bazel run "//solver:$SOLVER" -- --tgt_filename=$MLD_PATH/$mdl > $OUT_DIR/${output_file} || exit 1
+        echo "bazel run //solver:$SOLVER -- --tgt_filename=$MLD_PATH/$mdl > $OUT_DIR/${output_file} "
     fi
-done
-
+done | shuf | parallel -j ${P}
--- run_solver_fd.sh
+++ run_solver_fd.sh
@@ -7,11 +7,10 @@ echo $MDL_PATH $OUT_DIR $SOLVER
 
 mkdir -p $OUT_DIR
 
+P=${P:-1}
 for mdl in $MDL_PATH/FD*.mdl; do
     if [ -f $mdl ]; then
         output_file=`basename $mdl | sed -e 's/_src\.mdl/\.nbt/'`
-        echo "running for ${mdl}"
-        bazel run "//solver:$SOLVER" -- --src_filename=$MLD_PATH/$mdl > $OUT_DIR/${output_file} || exit 1
+        echo "bazel run //solver:$SOLVER -- --src_filename=$MLD_PATH/$mdl > $OUT_DIR/${output_file}"
     fi
-done
-
+done | shuf | parallel -j ${P} 
