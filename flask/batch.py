b7f83f1ab465451e3f854ec443c920ae97ea7a86
autotaker <autotaker@gmail.com>
2018-07-22 16:07:58 +0900

    multiprocessing

--- flask/batch.py
+++ flask/batch.py
@@ -7,6 +7,8 @@ import subprocess
 import json
 import traceback
 import time
+from multiprocessing import Process, Queue, Value
+from queue import Full
 
 dbconfig = {
     "database" : os.environ['DBNAME'],
@@ -15,6 +17,26 @@ dbconfig = {
     "host" : 'localhost',
 }
 destpath="static/solutions"
+
+task_queue = Queue(30)
+NUM_WORKERS = 8
+
+
+def worker(worker_id):
+    while True:
+        solution_id = task_queue.get()
+        print("worker %d is evaluating %d" % (worker_id, solution_id))
+        cnx = mysql.connector.connect(**dbconfig)
+        try:
+            eval_solution(cnx, solution_id)
+        except:
+            _,_, tb = sys.exc_info()
+            print("exception on worker ", worker_id, tb)
+            comment = str(tb)
+            curr=cnx.cursor()
+            curr.execute("update solutions set score = -1, comment = %s where id = %s ", (comment, solution_id))
+            curr.close()
+            cnx.commit()
 
 def eval_solution(cnx, solution_id):
     print("solution", solution_id)
@@ -60,21 +82,36 @@ def eval_solution(cnx, solution_id):
         cnx.commit()
 
 def main():
-    while True:
-        cnx = mysql.connector.connect(**dbconfig)
-        curr = cnx.cursor(dictionary=True)
-        curr.execute("select id from solutions where score is NULL order by created_at asc limit 1")
-        row = curr.fetchone()
-        if not row:
-            time.sleep(1)
-            continue
-        try: 
-            eval_solution(cnx, row['id'])
-        except:
-            traceback.print_exc()
-            curr.execute("update solutions set score = %s where id = %s", (-1,row['id']))
-            cnx.commit()
-            time.sleep(1)
+    # launch workers
+    workers = [ Process(target=worker,args=(i,)) for i in range(NUM_WORKERS) ] 
+    last_id = None
+    
+    for th in workers:
+        th.start()
+    
+    try:
+        while True:
+            cnx = mysql.connector.connect(**dbconfig)
+            curr = cnx.cursor(dictionary=True)
+            if last_id:
+                curr.execute("select id from solutions where score is NULL and id > %s order by id asc limit 1", (last_id,))
+            else:
+                curr.execute("select id from solutions where score is NULL order by id asc limit 1")
+            row = curr.fetchone()
+            curr.close()
+            cnx.close()
+            if not row:
+                time.sleep(1)
+                continue
+            try: 
+                task_queue.put(row['id'])
+                last_id = row['id']
+            except Full:
+                print("queue is full")
+                time.sleep(1)
+    finally:
+        for th in workers:
+            th.terminate()
 
 if __name__ == '__main__':
     main()
