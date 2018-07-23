f4b15322a0b0c9749323ddd8f02f94f946ccfeef
tikuta <tikuta@tikuta-Lenovo-ideapad-710S-Plus-13IKB>
2018-07-23 11:15:16 +0900

    fill

--- solver/AI.h
+++ solver/AI.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <memory>
+#include <vector>
+
 #include "src/base/base.h"
 #include "src/command_executer.h"
 
@@ -11,6 +13,10 @@ class AI {
   std::unique_ptr<CommandExecuter> ce;
  public:
  AI(vvv model) : ce(std::make_unique<CommandExecuter>(model, true)) {}
+
+  std::vector<Command> FillCommand(std::vector<Command> commands,
+                                   Command c) const;
+
   virtual ~AI() = default;
   virtual void Run() = 0;
   void Finalize() {
