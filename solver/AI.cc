51b5313d5cadd35798ff422fa084b0ae0ae56fd7
tikuta <tikuta@tikuta-Lenovo-ideapad-710S-Plus-13IKB>
2018-07-23 11:37:05 +0900

    ai_util

--- solver/AI.cc
+++ solver/AI.cc
@@ -17,3 +17,62 @@ std::vector<Command> AI::FillCommand(std::vector<Command> commands, Command fill
 
   return commands;
 }
+
+/* static */
+Command AI::GetStepSMove(int id, Point start, Point goal) {
+  if (start.x != goal.x) {
+    int d = std::min(kMAXSMOVE, std::abs(start.x - goal.x));
+    if (goal.x < start.x) d = -d;
+    Point p;
+    p.x = d;
+
+    return Command::make_smove(id, p);
+  }
+
+  if (start.y != goal.y) {
+    int d = std::min(kMAXSMOVE, std::abs(start.y - goal.y));
+    if (goal.y < start.y) d = -d;
+    Point p;
+    p.y = d;
+
+    return Command::make_smove(id, p);
+  }
+
+  if (start.z != goal.z) {
+    int d = std::min(kMAXSMOVE, std::abs(start.z - goal.z));
+    if (goal.z < start.z) d = -d;
+    Point p;
+    p.z = d;
+
+    return Command::make_smove(id, p);
+  }
+
+  LOG(FATAL) << "same point?";
+  return Command::make_wait(id);
+
+}
+
+/* static */
+std::vector<Command> AI::GetSMoves(int id, Point start, Point goal) {
+  std::vector<Command> smoves;
+
+  while (start != goal) {
+    auto smove = GetStepSMove(id, start, goal);
+    smoves.push_back(smove);
+    start += smove.smove_.lld;
+  }
+
+  return smoves;
+}
+
+
+std::vector<Command> AI::MultiStepSMove(const std::vector<std::pair<int, Point>>& targets) const {
+  std::vector<Command> command;
+
+  for (const auto& b : targets) {
+    if (bot(b.first).pos == b.second) continue;
+    command.push_back(GetStepSMove(b.first, bot(b.first).pos, b.second));
+  }
+  
+  return command;
+}
